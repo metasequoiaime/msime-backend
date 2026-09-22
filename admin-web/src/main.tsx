448ba897a840994282ce337577b0f3307ad1bca4
Evan Hu <e-hu@unext.jp>
2026-09-22 19:58:58 +0900

    build(admin): add Tailwind CSS pipeline

--- admin-web/src/main.tsx
+++ admin-web/src/main.tsx
@@ -10,6 +10,7 @@ import { SystemStatus } from "./system-status";
 import { ResourceList } from "./resource-list";
 import { isListPage } from "./pages";
 import "./style.scss";
+import "./tailwind.css";
 
 function NotFound() { return <section className="panel"><h1>页面不存在</h1><Link to="/">返回数据总览</Link></section>; }
 const rootRoute = createRootRoute({ component: Shell, notFoundComponent: NotFound });
